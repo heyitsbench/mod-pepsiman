c8ae5fd6be05dd437ac172d19919a6d0c146b68c
benjymansy123 <benjymansy123@gmail.com>
2023-03-21 15:39:10 -0400

    Initial commit III

--- src/pepsiman.cpp
+++ src/pepsiman.cpp
@@ -1,14 +1,88 @@
 #include "pepsiman_loader.h"
-#include "Chat.h"
-#include "Config.h"
 #include "Player.h"
-#include "ReputationMgr.h"
 #include "ScriptMgr.h"
+#include "ScriptedCreature.h"
 #include "TaskScheduler.h"
 
+enum creatureIDs
+{
+    GOOD_BENCH = 880000,
+    BAD_BENCH  = 880001
+};
 
+enum phaseEvents
+{
+    INTRO_EVENT         = 1,
+    MUSICAL_BENCH_START = 2,
+    MUSICAL_BENCHES     = 3,
+    SUPPORT_SPAWN       = 4
+};
+
+enum supportSpawns
+{
+    VENDOR_ENDERS = 1,
+    PEPPER_ADDS   = 2,
+    COCA_COLA     = 3
+};
+
+class pepsiman : public CreatureScript {
+
+public:
+    pepsiman() : CreatureScript("pepsiman") { };
+
+    struct pepsimanBoss : public ScriptedAI
+    {
+        pepsimanBoss(Creature* creature) : ScriptedAI(creature), summons(me) {}
+
+        void JustEngagedWith(Unit* who) override
+        {
+            ScriptedAI::JustEngagedWith(who);
+            events.ScheduleEvent(INTRO_EVENT, 2s, 0, 0);
+        }
+
+        void UpdateAI(uint32 diff) override
+        {
+            events.Update(diff);
+
+            while (uint32 eventID = events.ExecuteEvent())
+            {
+                switch (eventID)
+                {
+                case INTRO_EVENT:
+                    //yell something
+                    events.ScheduleEvent(MUSICAL_BENCH_START, 2s, 0, 0);
+                    break;
+                case MUSICAL_BENCH_START:
+                    if (!me->HealthBelowPct(75))
+                    {
+                        return;
+                    }
+                    //yell something
+                    //cast immunity spell or something
+                    events.ScheduleEvent(MUSICAL_BENCHES, 2s, 0, 0);
+                    break;
+                case MUSICAL_BENCHES:
+                    //yell something
+                    //spawn benches
+
+                    break;
+                }
+            }
+        }
+
+    private:
+        EventMap events;
+        SummonList summons;
+    };
+
+
+    CreatureAI* GetAI(Creature* creature) const override
+    {
+        return new pepsimanBoss(creature);
+    }
+};
 
 void Add_pepsiman()
 {
-
+    new pepsiman();
 }
