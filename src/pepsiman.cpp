fa3f89fdf70ba17102c71034dccbef6ee97ef6e4
benjymansy123 <benjymansy123@gmail.com>
2023-03-21 21:26:47 -0400

    Very WIP

--- src/pepsiman.cpp
+++ src/pepsiman.cpp
@@ -6,28 +6,49 @@
 
 enum creatureIDs
 {
-    GOOD_BENCH = 880000,
-    BAD_BENCH  = 880001
+    GOOD_BENCH     = 880000,
+    BAD_BENCH      = 880001,
+    COLA_CEO       = 880002,
+    MINI_ELEMENTAL = 880003,
+    DR_ELEMENTAL   = 880004
 };
 
 enum phaseEvents
 {
-    INTRO_EVENT         = 1,
-    MUSICAL_BENCH_START = 2,
-    MUSICAL_BENCHES     = 3,
-    SUPPORT_SPAWN       = 4
+    INTRO_EVENT           = 1,
+    CAST_BENCH            = 2,
+    MUSICAL_BENCH_START_1 = 3,
+    MUSICAL_BENCHES_1     = 4,
+    TRANSITION            = 5,
+    SUPPORT_SPAWN         = 6,
+    MUSICAL_BENCH_START_2 = 7,
+    MUSICAL_BENCHES_2     = 8,
+    ENRAGE                = 9
 };
 
 enum supportSpawns
 {
     VENDOR_ENDERS = 1,
     PEPPER_ADDS   = 2,
-    COCA_COLA     = 3
+    COCA_COLA     = 3,
+    WORGOBLIN     = 4
+};
+
+enum benchGObjects
+{
+    BENCHED_GOBJECT = 1,
+    NORMAL_BENCH    = 2,
+    CURSED_BENCH    = 3
 };
 
 enum spells
 {
     BENCH_STUN = 35856
+};
+
+enum talkMessages
+{
+    BENCH_ENGAGE = 0
 };
 
 class pepsiman : public CreatureScript {
@@ -39,10 +60,35 @@ public:
     {
         pepsimanBoss(Creature* creature) : ScriptedAI(creature), summons(me) {}
 
+        void EnterCombatSelfFunction()
+        {
+            Map::PlayerList const& PlList = me->GetMap()->GetPlayers();
+            if (PlList.IsEmpty())
+                return;
+
+            for (const auto& i : PlList)
+            {
+                if (Player* player = i.GetSource())
+                {
+                    if (player->IsGameMaster())
+                        continue;
+
+                    if (player->IsAlive() && me->GetDistance(player) < 50.0f)
+                    {
+                        me->SetInCombatWith(player);
+                        player->SetInCombatWith(me);
+                        me->AddThreat(player, 0.0f);
+                    }
+                }
+            }
+        }
+
         void JustEngagedWith(Unit* who) override
         {
             ScriptedAI::JustEngagedWith(who);
+            EnterCombatSelfFunction();
             events.ScheduleEvent(INTRO_EVENT, 2s, 0, 0);
+            Talk(BENCH_ENGAGE);
         }
 
         void Reset() override
@@ -53,6 +99,7 @@ public:
                 me->SetReactState(REACT_AGGRESSIVE);
             }
             events.Reset();
+            summons.DespawnAll();
             me->SendClearTarget();
         }
 
@@ -66,22 +113,98 @@ public:
                 {
                 case INTRO_EVENT:
                     //yell something
-                    events.ScheduleEvent(MUSICAL_BENCH_START, 2s, 0, 0);
-                    break;
-                case MUSICAL_BENCH_START:
+                    events.ScheduleEvent(CAST_BENCH, 10s, 0, 0);
+                    events.ScheduleEvent(MUSICAL_BENCH_START_1, 2s, 0, 0);
+                    return;
+                case CAST_BENCH:
+                    //yell mean words
+
+                    if (Unit* target = SelectTarget(SelectTargetMethod::Random, 0, 50.0f, true))
+                    {
+                        //cast stun
+                        me->SummonGameObject(BENCHED_GOBJECT, target->GetPositionX(), target->GetPositionY(), target->GetPositionZ(), target->GetOrientation(), 0.0f, 0.0f, 0.0f, 0.0f, 0);
+                    }
+                    events.Repeat(10s);
+                    return;
+                case MUSICAL_BENCH_START_1:
                     if (!me->HealthBelowPct(75))
                     {
                         return;
                     }
                     //yell something
-                    //cast immunity spell or something (setimmunetoall)
-                    events.ScheduleEvent(MUSICAL_BENCHES, 2s, 0, 0);
-                    break;
-                case MUSICAL_BENCHES:
+                    me->SetReactState(REACT_PASSIVE);
+                    me->AttackStop();
+                    me->SetImmuneToAll(true, true);
+
+                    events.DelayEvents(15s);
+                    events.ScheduleEvent(MUSICAL_BENCHES_1, 2s, 0, 0);
+                    return;
+                case MUSICAL_BENCHES_1:
                     //yell something
                     //spawn benches
 
-                    break;
+                    events.ScheduleEvent(TRANSITION, 8s, 0, 0);
+                    return;
+                case TRANSITION:
+                    //do a check to reduce player health, give buffs/debuffs
+
+                    me->RemoveAllGameObjects();
+                    me->SetReactState(REACT_AGGRESSIVE);
+                    me->SetInCombatWithZone();
+                    return;
+                case SUPPORT_SPAWN:
+                {
+                    if (!me->HealthBelowPct(50))
+                    {
+                        return;
+                    }
+
+                    uint8 supportSpawn = urand(1, 4);
+                    switch (supportSpawn)
+                    {
+                    case VENDOR_ENDERS:
+                        return;
+                    case PEPPER_ADDS:
+                        return;
+                    case COCA_COLA:
+                        return;
+                    case WORGOBLIN:
+                        return;
+                    }
+                    events.ScheduleEvent(MUSICAL_BENCH_START_2, 2s, 0, 0);
+                    return;
+                }
+                case MUSICAL_BENCH_START_2:
+                    if (!me->HealthBelowPct(30))
+                    {
+                        return;
+                    }
+                    //yell something
+
+                    events.DelayEvents(15s);
+                    events.ScheduleEvent(MUSICAL_BENCHES_2, 2s, 0, 0);
+                    return;
+                case MUSICAL_BENCHES_2:
+                    //yell something
+                    //spawn benches
+
+                    events.ScheduleEvent(ENRAGE, 2s, 0, 0);
+                    return;
+                case ENRAGE:
+                    if (!me->HealthBelowPct(10))
+                    {
+                        return;
+                    }
+                    events.CancelEvent(CAST_BENCH);
+                    //yell mean words
+                    
+                    if (Unit* target = SelectTarget(SelectTargetMethod::MaxThreat, 0, 50.0f, true))
+                    {
+                        //cast stun
+                        me->SummonGameObject(BENCHED_GOBJECT, target->GetPositionX(), target->GetPositionY(), target->GetPositionZ(), target->GetOrientation(), 0.0f, 0.0f, 0.0f, 0.0f, 0);
+                    }
+                    events.Repeat(5s);
+                    return;
                 }
             }
             if (!UpdateVictim())
